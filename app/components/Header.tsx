29318774d6e2a55ebeaa7a4cc2bbe54843612073
sstangg <sstang@bu.edu>
2025-05-01 00:32:06 -0400

    final working github login app

---
+++ app/components/Header.tsx
@@ -0,0 +1,7 @@
+export default function Header() {
+    return(
+        <header className="w-full bg-white p-4">
+            <a className="text-4x1 font-light p-4 text-black" href={"/"}>CS391 OAuth</a>
+        </header>
+    )
+}
---
+++ app/components/SignIn.tsx
@@ -0,0 +1,26 @@
+"use client"
+import { signIn, signOut, useSession } from "next-auth/react";
+
+export default function SignIn() {
+    const { data: session} = useSession()
+
+    return session ? (
+            <div className="bg-amber-200 text-black max-w-3xl w-full space-y-8 p-6 rounded-xl border-2">
+                <div className="text-center space-y-2 flex flex-col items-center">
+                    <h1 className=" text-3xl font-bold tracking-tight">
+                        {session.user?.email} <br />
+                        {session.user?.name}
+                        </h1>
+                    <img width={100} height={100} src={`${session.user?.image}`} alt={`${session.user?.name} logo`} />
+                    <button className="text-blue-950 p-2 bg-white border-2" onClick={() => signOut()}>Sign out</button>
+                </div>
+            </div>
+        ):(
+            <div className=" bg-amber-200 text-black max-w-3xl w-full space-y-8 p-6 rounded-xl border-2">
+                <div className="text-center space-y-2 flex flex-col items-center">
+                    <h1 className=" text-3xl font-bold tracking-tight">Auth.js Sign In</h1>
+                    <button className=" text-blue-950 p-2 bg-white border-2" onClick={() => signIn("github")}>Sign in with GitHub</button>
+                </div>
+            </div>
+        )
+}
